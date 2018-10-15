120d2545c63ef0acf01a7830f6cea92bd7f38c4b
jackyshyu <jackyshyu@gmail.com>
2018-10-15 14:14:41 -0500

    Create project

---
+++ SpringBootKKVideo97BatchCSVtoDB/src/main/java/org/shyu/springboot/SpringBootKkVideo97BatchCsVtoDbApplication.java
@@ -0,0 +1,12 @@
+package org.shyu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKkVideo97BatchCsVtoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKkVideo97BatchCsVtoDbApplication.class, args);
+	}
+}
